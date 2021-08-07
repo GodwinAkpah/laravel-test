39512f179c8ec8d0d8f6ada30aded35bc6cbcdc0
GodwinAkpah <gakpah96@gmail.com>
2021-08-07 15:42:20 +0100

    Added todo table

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\todos;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(todos $todos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(todos $todos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, todos $todos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\todos  $todos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(todos $todos)
+    {
+        //
+    }
+}
